e64431f493d7f9241f18c4fbc797bb514c07f763
Andreas Becher <a.becher@sportradar.com>
2026-03-05 08:15:43 +0100

    Update Package.swift for 1.0.1

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let coreSDKUrl = "https://github.com/sportradar/SRPlayer-SDK-Core"
 let coreSDKVersion : Version = "0.2.0"
 
 let nativeSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerSDK.xcframework.zip"
-let nativeSDKChecksum = "493fb799a29ea291bbac28185905a18e3d816b10009ed7edc5181d4fb80c6d19"
+let nativeSDKChecksum = "266b20908d090e9132ff50ab6ff117cb18c4a36ffd2283c9b05885f62143a4dd"
 
 let package = Package(
     name: "SRAVPlayerSDK",
