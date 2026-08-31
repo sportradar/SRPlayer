8f0983e3543036678798622ed958f9ff70eafcc4
Managed via Tart <admin@Manageds-Virtual-Machine.local>
2026-08-31 12:19:32 +0000

    Update Package.swift and DemoApp for 1.0.1

--- Package.swift
+++ Package.swift
@@ -5,13 +5,20 @@ let coreSDKUrl = "https://github.com/sportradar/SRPlayer-SDK-Core"
 let coreSDKVersion : Version = "0.3.49"
 
 let nativeSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerSDK.xcframework.zip"
-let nativeSDKChecksum = "68e1b7493cff2bc43790050fd7c728787efd0ce9c530b8e178a3fb093913efb2"
+let nativeSDKChecksum = "b2e359d1475c2d55940a50e219d0c9ed086b8a5740158016c9512bb7dc487353"
 
 let chromecastSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerChromecastPlugin.xcframework.zip"
-let chromecastSDKChecksum = "edaf6165677fb1c214b18041a3b569df633d318d1f4a4701cbd08268a2fe5309"
+let chromecastSDKChecksum = "60fb3cd88fa46e7cff518522aadded8f311e8342e485c5ea46fb105957d139fc"
 
 let googleCastUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/GoogleCast.xcframework.zip"
-let googleCastChecksum = "0f04603d36a43f4f561d4d54783e2b8ca52da9fc58e47fe1fc4bfc43e935701e"
+let googleCastChecksum = "9c797caa76694e42d836f2cbfa7f9dc22671f1dd5dea82120073bf0216b89b1c"
+
+let npawSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerNPAWPlugin.xcframework.zip"
+let npawSDKChecksum = "ecc9f9a20a10592070fd6d782f42be4ff060f70cb3b5668abf31b399bea74902"
+
+let npawPackageUrl = "https://bitbucket.org/npaw/plugin-ios.git"
+let npawPackageName = "plugin-ios"
+let npawPackageVersion : Version = "7.3.29"
 
 let package = Package(
     name: "SRAVPlayerSDK",
@@ -31,9 +38,14 @@ let package = Package(
             name: "SRAVPlayerChromecastPlugin",
             targets: ["SRAVPlayerChromecastTarget"]
         ),
+        .library(
+            name: "SRAVPlayerNPAWPlugin",
+            targets: ["SRAVPlayerNPAWTarget"]
+        ),
     ],
     dependencies: [
-        .package(url: coreSDKUrl, exact:coreSDKVersion)
+        .package(url: coreSDKUrl, exact:coreSDKVersion),
+        .package(url: npawPackageUrl, exact: npawPackageVersion),
     ],
     targets: [
         .binaryTarget(
@@ -51,6 +63,11 @@ let package = Package(
             url: googleCastUrl,
             checksum: googleCastChecksum
         ),
+        .binaryTarget(
+            name: "SRAVPlayerNPAWPlugin",
+            url: npawSDKUrl,
+            checksum: npawSDKChecksum
+        ),
         .target(
             name: "SRAVPlayerTarget",
             dependencies: [
@@ -67,6 +84,16 @@ let package = Package(
                 .target(name: "SRAVPlayerTarget"),
             ],
             path: "Sources/SRAVPlayerChromecastTarget"
+        ),
+        .target(
+            name: "SRAVPlayerNPAWTarget",
+            dependencies: [
+                // TODO: drop .when(platforms: [.iOS]) once build_framework.sh / project_template.yml emit macOS, tvOS, visionOS slices
+                .target(name: "SRAVPlayerNPAWPlugin", condition: .when(platforms: [.iOS])),
+                .product(name: "NpawPlugin", package: npawPackageName, condition: .when(platforms: [.iOS])),
+                .target(name: "SRAVPlayerTarget"),
+            ],
+            path: "Sources/SRAVPlayerNPAWTarget"
         )
     ]
 )
