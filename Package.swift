1c4aa5fa44064051fbc1deb9a44a8189f0eec38b
av-ios-sdk github_release <av-ios-sdk-ci@users.noreply.github.com>
2026-09-01 10:40:15 +0000

    Update Package.swift and DemoApp for 1.0.1

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ let coreSDKUrl = "https://github.com/sportradar/SRPlayer-SDK-Core"
 let coreSDKVersion : Version = "0.3.49"
 
 let nativeSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerSDK.xcframework.zip"
-let nativeSDKChecksum = "b2e359d1475c2d55940a50e219d0c9ed086b8a5740158016c9512bb7dc487353"
+let nativeSDKChecksum = "31960fa5f2dbd6291893acbbde17a3df96cf24dd1039ca95434cb8b8616c41aa"
 
 let chromecastSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerChromecastPlugin.xcframework.zip"
-let chromecastSDKChecksum = "60fb3cd88fa46e7cff518522aadded8f311e8342e485c5ea46fb105957d139fc"
+let chromecastSDKChecksum = "eab5c8b01c11fe62d7bac679b0f055928bf744afcbbb589b6962bd762f96647d"
 
 let googleCastUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/GoogleCast.xcframework.zip"
-let googleCastChecksum = "9c797caa76694e42d836f2cbfa7f9dc22671f1dd5dea82120073bf0216b89b1c"
+let googleCastChecksum = "80dba38046ae1c048099b4d99a2e2288119230471934c0dc62a4db075dc8d02b"
 
 let npawSDKUrl = "https://github.com/sportradar/SRPlayer/releases/download/1.0.1/SRAVPlayerNPAWPlugin.xcframework.zip"
-let npawSDKChecksum = "ecc9f9a20a10592070fd6d782f42be4ff060f70cb3b5668abf31b399bea74902"
+let npawSDKChecksum = "0de409f4cbbf1ead2bb32dd61fc3463341d868a5a9bb7b39a7b9b29537f3bf20"
 
 let npawPackageUrl = "https://bitbucket.org/npaw/plugin-ios.git"
 let npawPackageName = "plugin-ios"
